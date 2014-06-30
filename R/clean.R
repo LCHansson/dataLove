8e5fff7f1ceab57f8b25011070fe977e06f14eac
LCHansson <love.hansson@gmail.com>
2014-07-01 01:35:06 +0200

    Basic Umlaut cleaning in vectors

--- R/clean.R
+++ R/clean.R
@@ -8,4 +8,24 @@ cleanData <- function(.data, .class = NULL, .action = NULL) {
   NULL
 }
 
-
+#' @export
+fixUmlauts <- function(.data, src = "åäöüß", target = "ASCII") {
+  # Error handling (TODO: Implement support for tabular data)
+  if (!is.vector(.data)) stop("Data not a vector!")
+  
+  dict <- list(
+    "å" = "a",
+    "ä" = "a",
+    "ö" = "o",
+    "ü" = "u",
+    "ß" = "ss"
+  )
+  
+  if (target == "ASCII") {
+    for (char in names(dict)) {
+      .data <- str_replace_all(.data, char, dict[[char]])
+    }
+  }
+  
+  return(.data)
+}
